41d3bf89c51b8027e9cfc37ef2976befca9338d5
Dan Olver <devdolphin7@outlook.com>
2024-06-26 10:04:42 +0100

    Sucessfully getting user input

--- cat_mouse_game.js
+++ cat_mouse_game.js
@@ -1,16 +1,43 @@
+const inquirer = require("inquirer");
+
 class CatNMouse {
-    loadGame(grid=[["cat", "x", "O", "mouse"]], catMovement=2, mouseMovement=1) {
-        this.gameGrid = grid
-        this.catMovement = catMovement
-        this.mouseMovement = mouseMovement
-        console.log("Welcome to cat catcher!\n\n")
-        console.log(`Cat movement: ${this.catMovement}, mouse movement: ${this.mouseMovement}\n`)
-        console.log(this.gameGrid)
-}
+    loadGame(
+        grid = [["cat", "x", "O", "mouse"]],
+        catMovement = 2,
+        mouseMovement = 1
+    ) {
+        this.gameGrid = grid;
+        this.catMovement = catMovement;
+        this.mouseMovement = mouseMovement;
+        console.log("Welcome to cat catcher!\n");
+        console.log(
+            `Cat movement: ${this.catMovement}, mouse movement: ${this.mouseMovement}\n`
+        );
+        console.log(this.gameGrid);
+    }
 
     playGame() {
-        this.input = prompt("What is your move?")
+        this.getUserMove();
+    }
+
+    getUserMove() {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "userMove",
+                    message: "Use <wasd> to move around the grid: ",
+                    validate: input => /^[wasd]{1}$/i.test(input),
+                },
+            ])
+            .then((answer) => {
+                console.log(answer);
+            });
     }
 }
 
-module.exports = CatNMouse
+let instance = new CatNMouse();
+instance.loadGame();
+instance.playGame();
+
+module.exports = CatNMouse;
